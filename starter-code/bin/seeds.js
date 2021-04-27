fd49b49af22684c9ab5072cc6146361cab9a7f52
marina <marinaaltesmagret@gmail.com>
2021-04-27 16:41:27 +0200

    Iteration 7

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,20 +1,39 @@
 const mongoose = require('mongoose');
-const Celebrity = require ('../models/Celebrity.model')
-const celebritiesArr = [
+// const Celebrity = require ('../models/Celebrity.model')
+// const celebritiesArr = [
+//     {
+//         name: 'Uma Thurman',
+//         occupation: 'actress, ninja',
+//         catchPhrase: 'Gimme some Bill' 
+//     },
+//     {
+//         name: 'Charlotte Gainsbourg',
+//         occupation: 'actress, singer',
+//         catchPhrase: 'Too cool for school' 
+//     },
+//     {
+//         name: 'Henry Cavill',
+//         occupation: 'actor, bodybuilder',
+//         catchPhrase: 'To infinity and beyond' 
+//     }
+// ]
+
+const Movie = require('../models/Movie.model');
+const moviesArr = [
     {
-        name: 'Uma Thurman',
-        occupation: 'actress, ninja',
-        catchPhrase: 'Gimme some Bill' 
+        title: 'Titanic',
+        genre: 'Drama',
+        plot: 'People go on a big ship, ship hits an iceberg, ship sinks in the ocean, nearly all die.'
     },
     {
-        name: 'Charlotte Gainsbourg',
-        occupation: 'actress, singer',
-        catchPhrase: 'Too cool for school' 
+        title: 'Matrix',
+        genre: 'Science-fiction',
+        plot: 'People discover that their lives are fake, and they are controlled by machines.'
     },
     {
-        name: 'Henry Cavill',
-        occupation: 'actor, bodybuilder',
-        catchPhrase: 'To infinity and beyond' 
+        title: 'West Side Story',
+        genre: 'Musical',
+        plot: 'Romeo and Juliet version with cool songs and dances.'
     }
 ]
 
@@ -24,7 +43,7 @@ mongoose
         useUnifiedTopology: true,
         useCreateIndex: true
     })
-    .then(() => Celebrity.create(celebritiesArr))
-    .then((celebs) => console.log(`${celebs.length} created`))
+    .then(() => Movie.create(moviesArr))
+    .then((movies) => console.log(`${movies.length} created`))
     .catch(err => console.log('Error seeding: ', err))
     .finally(()=> mongoose.connection.close())
