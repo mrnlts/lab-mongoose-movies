33f67945660892f458a7f1b90c0faeeedbf172dd
marina <marinaaltesmagret@gmail.com>
2021-04-27 17:08:29 +0200

    Iteration 10

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -16,6 +16,27 @@ router.get('/movies', (req, res, next) => {
         })
 })
 
+
+/* Create new celebrity */
+
+router.get('/movies/new', (req, res, next) => {
+    console.log('Enter new movie');
+    res.render('movies/new');
+})
+
+router.post('/movies', (req, res, next) => {
+    const { title, genre, plot } = req.body;
+    Movie.create({ title, genre, plot})
+        .then((newMovie) => {
+            newMovie.save();
+            res.redirect('/movies');
+        })
+        .catch((err) => {
+            console.log('Error adding new Movie to DB: ', err);
+            res.render('movies/new');
+        })
+})
+
 /* Show movie's details */
 
 router.get('/movies/:id', (req, res, next) => {
