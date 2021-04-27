51f737128ba23ed8986feee861be6fcf23c57301
marina <marinaaltesmagret@gmail.com>
2021-04-27 13:23:01 +0200

    Iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -9,12 +9,21 @@ const router = express.Router();
 router.get('/celebrities', (req, res, next) => {
     console.log('Celebrities page');
     Celebrity.find()
-        .then((celebrities)=> {
-            console.log({celebrities});
-            res.render('celebrities/index', {celebs: celebrities})
-        })
+        .then((celebrities)=> res.render('celebrities/index', {celebs: celebrities}))
         .catch(err => {
             console.log('Error looking for celebrities: ', err);
+            next(err);
+        })
+})
+
+/* Show celebrity's details */
+
+router.get('/celebrities/:id', (req, res, next) => {
+    const {id} = req.params;
+    Celebrity.findOne({'_id': id})
+        .then((celebFromDB)=> res.render('celebrities/show', celebFromDB))
+        .catch(err => {
+            console.log('Error displaying details of celebrity: ', err);
             next(err);
         })
 })
